2e079ea12a153969881b0044e54389e9df58b3f3
Jirlon Oliveira <jirlon_jiw@hotmail.com>
2022-08-16 19:58:11 -0300

    primeiro commit

---
+++ app/src/main/java/controller/TaskController.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import model.Task;
+import util.ConnectionFactory;
+
+/**
+ *
+ * @author jirlo
+ */
+public class TaskController {
+    
+    public void save(Task task){
+        String sql = "INSERT INTO tasks (idProject"
+                + "name,"
+                + "description,"
+                + "completed,"
+                + "notes,"
+                + "deadline,"
+                + "createdAt,"
+                + "updatedAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try {
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, task.getIdProject());
+            statement.setString(2, task.getName());
+            statement.setString(3, task.getDescription());
+            statement.setBoolean(4, task.isIsCompleted());
+            statement.setString(5, task.getNotes());
+            statement.setDate(6, new Date(task.getCreatedAt().getTime()));
+            statement.setDate(7, new Date(task.getCreatedAt().getTime()));
+            statement.setDate(8, new Date(task.getUpdateAt().getTime()));
+            statement.execute();
+        } catch (Exception ex)   {
+            throw new RuntimeException("Erro ao salvar a tarefa"
+                + ex.getMessage(), ex);
+            }   finally {
+            connectionFactory.closeConnection(connection);
+        }
+             
+    }
+    
+    public void update(Task task){
+    
+    }
+    
+    public void removeById(int taskId) throws SQLException{
+        String sql = "DELETE FROM tasks WHERE id = ?";
+        
+        Connection conn = null;
+        PreparedStatement statement = null;
+        
+        try {
+            conn = ConnectionFactory.getConnection();
+            statement = conn.prepareStatement(sql);
+            statement.setInt(1, taskId);
+            statement.execute();
+        } catch (SQLException e){
+            throw new SQLException("Erro ao deletar a tarefa");
+            } finally {
+                ConnectionFactory.closeConnection(conn);
+            }
+    }
+    
+    public List<Task> getAll(int idProject){
+        return null;
+    }
+}
