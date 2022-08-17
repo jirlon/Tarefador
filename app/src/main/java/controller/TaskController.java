ae0f3b035dc0092fef5f3285f0034e3d7d04d694
Jirlon Oliveira <jirlon_jiw@hotmail.com>
2022-08-17 17:09:04 -0300

    metodo getAll

--- app/src/main/java/controller/TaskController.java
+++ app/src/main/java/controller/TaskController.java
@@ -7,7 +7,9 @@ package controller;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import model.Task;
 import util.ConnectionFactory;
@@ -77,7 +79,8 @@ public class TaskController {
             statement.setBoolean(5, task.isIsCompleted());
             statement.setDate(6, new Date(task.getDeadline().getTime()));
             statement.setDate(7, new Date(task.getCreatedAt().getTime()));
-            statement.setDate(9, new Date(task.getUpdateAt().getTime()));
+            statement.setDate(8, new Date(task.getUpdateAt().getTime()));
+            statement.setInt(9, task.getId());
             statement.execute();
         }    catch (Exception ex){
                 throw new RuntimeException("Erro ao atualizar a tarefa" + ex.getMessage(), ex);
@@ -104,6 +107,43 @@ public class TaskController {
     }
     
     public List<Task> getAll(int idProject){
-        return null;
+        String sql = "SELECT * FROM tasks WHERE idProject = ?";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        
+        //lista de tarefas que sera devolvida quando a chamada do metodo acontecer
+        List<Task> tasks = new ArrayList<Task>();
+        
+        try {
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, idProject);
+            resultSet = statement.executeQuery();
+            
+            while(resultSet.next()){
+                Task task = new Task();
+                task.setId(resultSet.getInt("id"));
+                task.setIdProject(resultSet.getInt("idProject"));
+                task.setName(resultSet.getString("name"));
+                task.setDescription(resultSet.getString("description"));
+                task.setNotes(resultSet.getString("notes"));
+                task.setIsCompleted(resultSet.getBoolean("completed"));
+                task.setDeadline(resultSet.getDate("deadline"));
+                task.setCreatedAt(resultSet.getDate("createdAt"));
+                task.setUpdateAt(resultSet.getDate("updatedAt"));
+                
+                tasks.add(task);
+            }
+            
+        } catch (Exception ex){
+            throw new RuntimeException("Erro ao inserir a tarefa" + ex.getMessage(), ex);
+            } finally {
+                ConnectionFactory.closeConnection(connection, statement, resultSet);
+            }
+        
+        //retornando lista de tarefas do banco de dados
+        return tasks;
     }
 }
